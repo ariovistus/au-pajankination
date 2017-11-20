a098bc87c5b6e8800c19cafc7032555f4c1dbc0c
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-11-20 15:57:29 -0800

    still buggy

--- samples/requirejs-sample/src/data-source.ts
+++ samples/requirejs-sample/src/data-source.ts
@@ -12,25 +12,14 @@ export class SpoofedDataSource implements IComboboxDataSource {
     }
 
     public getPage(take: number, skip: number, searchText: string): Promise<IComboboxResults> {
-        let actualTake = Math.min(take, 100);
-
         var page = [];
-        var searchStart = parseInt(searchText) || 0;
-        var start = skip + searchStart;
-        for(var i = start; i < start + actualTake; i++) {
-            let obj = {
-                id: i
-            };
-            page.push(obj);
-        }
+        page.push( {
+                id: 1
+            });
 
-        return new Promise(function(resolve) {
-            setTimeout(function() {
-                resolve({
-                    count: 100000,
-                    rows: page
-                });
-            }, 34);
+        return Promise.resolve({
+            count: 1,
+            rows: page
         });
     }
 }

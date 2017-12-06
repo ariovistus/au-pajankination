77687ff2bc39a7dca41b78e2f37a207e7b82a239
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-12-06 12:25:15 -0800

    update build

--- build_npm.sh
+++ build_npm.sh
@@ -2,7 +2,8 @@ rm -rf dist/*
 mkdir -p dist 
 cp src/select2.css dist
 cp src/*.html dist
-docker run -v $(pwd):/app --entrypoint yarn sandrokeil/typescript install
-docker run -v $(pwd):/app --entrypoint python sandrokeil/typescript versionchecker.py
-docker run -v $(pwd):/app --entrypoint tsc sandrokeil/typescript -d -p . --outDir dist
-docker run -v $(pwd):/app --entrypoint npm sandrokeil/typescript pack
+docker run -v "$(pwd):/app:z" --entrypoint yarn sandrokeil/typescript install
+docker run -v "$(pwd):/app:z" --entrypoint python sandrokeil/typescript versionchecker.py
+docker run -v "$(pwd):/app:z" --entrypoint tsc sandrokeil/typescript -d -p . --outDir dist
+docker run -v "$(pwd):/app:z" --entrypoint sh sandrokeil/typescript build_tsdecls_only.sh 
+docker run -v "$(pwd):/app:z" --entrypoint npm sandrokeil/typescript pack
